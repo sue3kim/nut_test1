cea79a3bc9d0a17ee7d1fe7ec47dcbc682be0974
Suhyeon Kim <162249386+sue3kim@users.noreply.github.com>
2024-08-05 19:16:56 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.36.48.172']
 
